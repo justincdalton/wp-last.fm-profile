6cfb95616214116874a0d4683bad814e9c03d82b
justincdalton <jdalton15@gmail.com>
2013-07-22 00:31:08 -0400

    Added handlebars for html templating

--- js/lastfm-profile.js
+++ js/lastfm-profile.js
@@ -1,27 +1,35 @@
 
 var lastfmProfile = function() {
-	$ = jQuery;
-	var lastfmProfileDiv, 
+	// Initialize local variables
+	var $,
+		lastfmProfileDiv, 
 		artistDisplayCount, 
 		handlebarsSource, 
 		handlebarsTemplate, 
 		handlebarsContext, 
 		handlebarsHtml;
 
+	$ = jQuery;
+
+	// Init function for lastfmProfile plugin js
 	var init = function(_artistDisplayCount) {
+		// Set local variables
 		lastfmProfileDiv = $('#lastfm_profile');
 		artistDisplayCount = _artistDisplayCount;
-		handlebarsSource = $('#lastfm-profile-handlebars').html();
+
+		// Set handlebars variables
+		handlebarsSource = $('#lastfm-profile-artists-handlebars').html();
 		handlebarsTemplate = Handlebars.compile(handlebarsSource);
 
 		getTopArtists();
 	}
 
+	// 
 	var getTopArtists = function() {
 		$.ajax({
 			url: 'http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=pablious&api_key=0c48e23cbb8d3bc9e2268146c1f520d7&format=json&limit=' + artistDisplayCount
 		}).done(function(data) {
-			handlebarsContext = { artists: data.topartists.artist };
+			handlebarsContext = { data: data };
 			handlebarsHtml = handlebarsTemplate(handlebarsContext);
 			lastfmProfileDiv.append(handlebarsHtml);
 		});
--- wp-lastfm-profile.php
+++ wp-lastfm-profile.php
@@ -20,6 +20,9 @@ $artistDisplayCount = 12;
 //include('wp-lastfm-profile-admin.php');
 
 
+/**
+ * Register scripts including Handlebars.js and the plugin script
+ */
 function lastfm_profile_scripts() {
 	wp_register_script('handlebars_js', plugin_dir_url(__FILE__) . 'js/handlebars.js');
 	wp_enqueue_script('handlebars_js');
@@ -29,17 +32,26 @@ function lastfm_profile_scripts() {
 
 add_action('wp_enqueue_scripts', 'lastfm_profile_scripts');
 
+
+/**
+ * Insert this method into a template to return base plugin html.
+ * Includes a Handlebars.js template which will render json of artists returned by Last.fm.
+ */
 function lastfm_profile_display() {
 	?>
 	<script id="lastfm-profile-handlebars" type="text/x-handlebars-template">
-		<ul>
-			{{#each artists}}
-				<li>
-					<img src="{{image.[0].[#text]}}" alt="{{name}}" />
-					<h4>{{name}}</h4>
-				</li>
-			{{/each}}
-		</ul>
+		{{#if data.error}}
+			<h4>An error has occurred retrieving Last.fm data</h4>
+		{{else}}
+			<ul>
+				{{#each data.topartists.artist}}
+					<li>
+						<img src="{{image.[0].[#text]}}" alt="{{name}}" />
+						<h4>{{name}}</h4>
+					</li>
+				{{/each}}
+			</ul>
+		{{/if}}
 	</script>
 	<div id="lastfm_profile"></div>
 	<?php
